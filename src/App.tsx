9fe480e9bf213d279da73dcc04a2f5e309d79f72
ronit-ghosh <ronitghosh06@gmail.com>
2024-10-22 21:19:46 +0530

    Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
 
   return (
     <div className="h-dvh w-full flex flex-col gap-10 justify-center items-center">
-      <div className="flex gap-10">
+      <div className="flex sm:flex-row flex-col gap-10 items-center">
         <input
           onChange={onUrlChange}
           value={url}
@@ -55,14 +55,14 @@ export default function App() {
           placeholder="Write here..."
           className="bg-gray-300 outline outline-3 outline-offset-2 outline-yellow-400 rounded-lg p-3" />
         <select
-          className="bg-gray-300 outline outline-3 outline-offset-2 outline-yellow-400 rounded-lg px-3"
+          className="bg-gray-300 outline outline-3 outline-offset-2 outline-yellow-400 rounded-lg px-3 sm:py-0 py-2"
           onChange={onExtensionChange}>
           <option className="" value="jpeg">JPEG</option>
           <option className="" value="png">PNG</option>
           <option className="" value="webp">WEBP</option>
         </select>
         <button
-          className="bg-gray-300 outline outline-3 outline-offset-2 outline-yellow-400 rounded-lg px-3"
+          className="bg-gray-300 outline outline-3 outline-offset-2 outline-yellow-400 rounded-lg px-3 sm:py-0 py-2"
           onClick={onDownloadClick}>Download</button>
       </div>
       <div ref={ref} />
